e33db9c319a85457e5f15c763430289c5c26d0a0
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2025-04-12 00:03:31 -0300

    feat: :sparkles: Adiciona ConfigModule e arquivo de configuração para gerenciar variáveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import envConfig from './config/env-config';
 import { UsersModule } from './interface/modules/user.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb://root:example@localhost:27017/nestjs?authSource=admin',
-    ),
+    ConfigModule.forRoot({
+      load: [envConfig],
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI || ''),
     UsersModule,
   ],
   controllers: [],
