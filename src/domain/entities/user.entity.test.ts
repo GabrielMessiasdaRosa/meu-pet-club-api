5d4b7ed7c07b079fd89f384281646dcfb381c06d
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2025-04-13 23:19:24 -0300

    feat: :sparkles: Adiciona novos testes para gerenciamento de resetToken e atualização de dados do usuário

--- src/domain/entities/user.entity.test.ts
+++ src/domain/entities/user.entity.test.ts
@@ -194,4 +194,130 @@ describe('User Entity', () => {
 
     expect(() => new User(userData)).toThrow('Token de redefinição inválido');
   });
+
+  it('Deve retornar o resetTokenExpires corretamente', () => {
+    const expirationDate = new Date();
+    const validResetToken = randomUUID();
+    const userData = {
+      id: '129',
+      name: 'Token Expiration User',
+      email: 'token.expiration@example.com',
+      password: 'password',
+      role: RoleEnum.USER,
+      resetToken: validResetToken,
+      resetTokenExpires: expirationDate,
+    };
+
+    const user = new User(userData);
+    expect(user.ResetTokenExpires).toBe(expirationDate);
+  });
+
+  it('Deve atualizar o resetToken com sucesso', () => {
+    const userData = {
+      id: '130',
+      name: 'Update Token User',
+      email: 'update.token@example.com',
+      password: 'password',
+      role: RoleEnum.USER,
+    };
+
+    const user = new User(userData);
+    const validToken = randomUUID();
+    user.setResetToken(validToken);
+    expect(user.ResetToken).toBe(validToken);
+  });
+
+  it('Deve lançar um erro ao tentar definir um resetToken nulo', () => {
+    const userData = {
+      id: '131',
+      name: 'Null Token User',
+      email: 'null.token@example.com',
+      password: 'password',
+      role: RoleEnum.USER,
+    };
+
+    const user = new User(userData);
+    expect(() => user.setResetToken(null)).toThrow(
+      'Token de redefinição é obrigatório',
+    );
+  });
+
+  it('Deve atualizar a data de expiração do token com sucesso', () => {
+    const userData = {
+      id: '132',
+      name: 'Update Expiration User',
+      email: 'update.expiration@example.com',
+      password: 'password',
+      role: RoleEnum.USER,
+      resetToken: randomUUID(),
+      resetTokenExpires: new Date(),
+    };
+
+    const user = new User(userData);
+    const newExpirationDate = new Date(2025, 4, 20);
+    user.setResetTokenExpires(newExpirationDate);
+    expect(user.ResetTokenExpires).toBe(newExpirationDate);
+  });
+
+  it('Deve lançar um erro ao tentar definir uma data de expiração nula', () => {
+    const userData = {
+      id: '133',
+      name: 'Null Expiration User',
+      email: 'null.expiration@example.com',
+      password: 'password',
+      role: RoleEnum.USER,
+      resetToken: randomUUID(),
+      resetTokenExpires: new Date(),
+    };
+
+    const user = new User(userData);
+    expect(() => user.setResetTokenExpires(null)).toThrow(
+      'Data de expiração é obrigatória',
+    );
+  });
+
+  it('Deve atualizar o nome com sucesso', () => {
+    const userData = {
+      id: '134',
+      name: 'Nome Original',
+      email: 'user@example.com',
+      password: 'password123',
+      role: RoleEnum.USER,
+    };
+
+    const user = new User(userData);
+    const novoNome = 'Nome Atualizado';
+    user.setName(novoNome);
+    expect(user.Name).toBe(novoNome);
+  });
+
+  it('Deve atualizar o email com sucesso', () => {
+    const userData = {
+      id: '135',
+      name: 'Test User',
+      email: 'original@example.com',
+      password: 'password123',
+      role: RoleEnum.USER,
+    };
+
+    const user = new User(userData);
+    const novoEmail = 'atualizado@example.com';
+    user.setEmail(novoEmail);
+    expect(user.Email).toBe(novoEmail);
+  });
+
+  it('Deve atualizar a senha com sucesso', () => {
+    const userData = {
+      id: '136',
+      name: 'Password User',
+      email: 'pass.user@example.com',
+      password: 'senhaOriginal',
+      role: RoleEnum.USER,
+    };
+
+    const user = new User(userData);
+    const novaSenha = 'novaSenhaSegura123';
+    user.setPassword(novaSenha);
+    expect(user.Password).toBe(novaSenha);
+  });
 });
